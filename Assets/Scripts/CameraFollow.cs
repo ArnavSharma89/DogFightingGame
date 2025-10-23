a6207b414deb5f5b4b770fe2a6e3e6261f1d0b9a
Arnavexe <arnavsharma0896@gmail.com>
2025-10-23 16:54:14 +0530

    Added a camera follow script
    The Main camera now follows the plane in a TPP mode

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
+    //the difference between the position of the camera and the player;
     Vector3 offset;
+
+    //Main Camera of the scene;
     public Camera main;
 
     void Start() {
